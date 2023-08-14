77cb3e95e1c191056d00847bdc696ab83fd8ed47
caduque0906 <caduque0906@gmail.com>
2023-08-14 16:23:23 -0400

    Getting details for Person 1

--- MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
+++ MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -13,7 +13,12 @@ namespace MathAndComparisonOperators
             // Print the program header
             Console.WriteLine("Anonymous Income Comparison Program");
 
-            
+            // Get details for Person 1
+            Console.WriteLine("Person 1");
+            Console.Write("Hourly Rate? ");
+            double hourlyRatePerson1 = Convert.ToDouble(Console.ReadLine());  // Get hourly rate as a double
+            Console.Write("Hours worked per week? ");
+            double hoursWorkedPerWeekPerson1 = Convert.ToDouble(Console.ReadLine());  // Get hours worked per week as a double
         }
     }
 }
