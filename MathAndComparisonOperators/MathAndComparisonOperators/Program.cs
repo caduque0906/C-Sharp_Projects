0f6b6adbb9a32dd708ccef6731b8d7cd79003d1e
caduque0906 <caduque0906@gmail.com>
2023-08-14 16:22:09 -0400

    Added program header

---
+++ MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the program header
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            
+        }
+    }
+}
