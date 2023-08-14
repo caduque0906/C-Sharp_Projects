58c95640874da5ef43d5954ac34a5a629438c2c4
caduque0906 <caduque0906@gmail.com>
2023-08-14 16:25:01 -0400

    Added annual salaries and comparison result.

--- MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
+++ MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -27,6 +27,25 @@ namespace MathAndComparisonOperators
             double hourlyRatePerson2 = Convert.ToDouble(Console.ReadLine());  // Get hourly rate as a double
             Console.Write("Hours worked per week? ");
             double hoursWorkedPerWeekPerson2 = Convert.ToDouble(Console.ReadLine());  // Get hours worked per week as a double
+
+
+            // Calculate annual salaries for both persons
+            double annualSalaryPerson1 = hourlyRatePerson1 * hoursWorkedPerWeekPerson1 * 52;
+            double annualSalaryPerson2 = hourlyRatePerson2 * hoursWorkedPerWeekPerson2 * 52;
+
+            // Print annual salaries
+            Console.WriteLine("Annual salary of Person 1:");
+            Console.WriteLine(annualSalaryPerson1.ToString("N"));  // Display salary with thousands separator
+            Console.WriteLine("Annual salary of Person 2:");
+            Console.WriteLine(annualSalaryPerson2.ToString("N"));  // Display salary with thousands separator
+
+            // Check if Person 1 makes more money than Person 2
+            bool person1MakesMore = annualSalaryPerson1 > annualSalaryPerson2;
+
+            // Print the comparison result
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            Console.WriteLine(person1MakesMore);
+            Console.ReadLine();
         }
     }
 }
