62ef7378397614960a14a0004b990e0f3d14a90c
caduque0906 <caduque0906@gmail.com>
2023-08-16 15:34:24 -0400

    First commit

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Boolean comparison using a while statement
+            Console.WriteLine("Boolean comparison using a while statement:");
+            int counterWhile = 0;  // Initialize counter
+            while (counterWhile < 5)  // Loop while counter is less than 5
+            {
+                Console.WriteLine($"Counter: {counterWhile}");
+                counterWhile++;  // Increment counter
+            }
+
+            // Boolean comparison using a do while statement
+            Console.WriteLine("Boolean comparison using a do while statement:");
+            int counterDoWhile = 0;  // Initialize counter
+            do
+            {
+                Console.WriteLine($"Counter: {counterDoWhile}");
+                counterDoWhile++;  // Increment counter
+            } while (counterDoWhile < 5);  // Loop while counter is less than 5
+            Console.ReadLine();
+        }
+    }
+}
