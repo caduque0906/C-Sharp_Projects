fc4b612f0947b8c370d9d18193b4105ec46b28eb
caduque0906 <caduque0906@gmail.com>
2023-08-20 15:23:37 -0400

    Added parts 4-6

--- ConsoleAppTest/ConsoleAppTest/Program.cs
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -91,6 +91,57 @@ namespace ConsoleAppTest
             }
 
             Console.ReadLine();
+
+            // Create a list of strings with at least two identical strings
+            List<string> stringList2 = new List<string> { "Apple", "Banana", "Cherry", "Banana", "Grape" };
+
+            // Ask the user to input text to search for
+            Console.Write("Enter text to search for: ");
+            string searchText2 = Console.ReadLine();
+
+            bool found2 = false;  // Flag to indicate if a match has been found
+
+            // Loop to iterate through the list
+            for (int i = 0; i < stringList2.Count; i++)
+            {
+                if (stringList2[i] == searchText2)
+                {
+                    Console.WriteLine($"Match found at index {i}");
+                    found2 = true;  // Match found, set flag to true
+                }
+            }
+
+            // Check if a match was not found
+            if (!found2)
+            {
+                Console.WriteLine("Input is not on the list.");
+            }
+
+            Console.ReadLine();
+
+            // Create a list of strings with at least two identical strings
+            List<string> stringList3 = new List<string> { "A", "B", "C", "D", "C" };
+
+            // Create a dictionary to keep track of string occurrences
+            Dictionary<string, int> occurrences = new Dictionary<string, int>();
+
+            // Loop to evaluate each item in the list using a foreach loop
+            foreach (string item in stringList3)
+            {
+                // Check if the item is already in the dictionary
+                if (occurrences.ContainsKey(item))
+                {
+                    occurrences[item]++;  // Increment the count if it's a duplicate
+                    Console.WriteLine($"{item} - this item is a duplicate (occurrences: {occurrences[item]})");
+                }
+                else
+                {
+                    occurrences[item] = 1;  // Add the item to the dictionary with a count of 1
+                    Console.WriteLine($"{item} - this item is unique");
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
