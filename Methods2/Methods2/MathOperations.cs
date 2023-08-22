a4bc4274542092eb8b62b29e96889cb5f37b8d56
caduque0906 <caduque0906@gmail.com>
2023-08-22 08:06:16 -0400

    Added Math operations class

---
+++ Methods2/Methods2/MathOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    public class MathOperations
+    {
+        public void DisplayMathResult(int num1, int num2)
+        {
+            // Do a math operation on the first number
+            int result = num1 * 2;
+
+            // Display the second number to the screen
+            Console.WriteLine($"Second number: {num2}");
+        }
+    }
+}
