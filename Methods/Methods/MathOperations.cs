66964c2530ffe971e04239673b345c389d530a20
caduque0906 <caduque0906@gmail.com>
2023-08-22 07:58:09 -0400

    First Commit

---
+++ Methods/Methods/MathOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class MathOperations
+    {
+        public int PerformMath(int num1, int num2 = 0)
+        {
+            // Do a math operation (addition in this case)
+            int result = num1 + num2;
+            return result;
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the MathOperations class
+            MathOperations mathOps = new MathOperations();
+
+            // Ask the user to input two numbers
+            Console.Write("Enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number (leave empty if not entering): ");
+            string input = Console.ReadLine();
+            int num2 = string.IsNullOrEmpty(input) ? 0 : Convert.ToInt32(input);
+
+            // Call the method and display the result
+            int result = mathOps.PerformMath(num1, num2);
+            Console.WriteLine($"Result of PerformMath: {result}");
+
+            Console.ReadLine();
+        }
+    }
+}
