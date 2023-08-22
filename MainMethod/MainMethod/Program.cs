c0ed2910376e245d194f3721fdc2f8dcbf1ad425
caduque0906 <caduque0906@gmail.com>
2023-08-21 21:56:11 -0400

    Created parameters to display results.

--- MainMethod/MainMethod/Program.cs
+++ MainMethod/MainMethod/Program.cs
@@ -10,6 +10,22 @@ namespace MainMethod
     {
         static void Main(string[] args)
         {
+            // Create an instance of the MathOperations class
+            MathOperations mathOps = new MathOperations();
+
+            // Call the first method with an integer parameter
+            int result1 = mathOps.PerformMath(5);
+            Console.WriteLine($"Result of PerformMath with integer: {result1}");
+
+            // Call the second method with a decimal parameter
+            int result2 = mathOps.PerformMath(16.25m);
+            Console.WriteLine($"Result of PerformMath with decimal: {result2}");
+
+            // Call the third method with a string parameter
+            int result3 = mathOps.PerformMath("20");
+            Console.WriteLine($"Result of PerformMath with string: {result3}");
+
+            Console.ReadLine();
         }
     }
 }
