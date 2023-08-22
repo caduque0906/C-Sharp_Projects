f44514a68defea0474ba23e4252f269dbc9bad55
caduque0906 <caduque0906@gmail.com>
2023-08-22 08:22:44 -0400

    Created Math Operations class

---
+++ ClassAssignment/ClassAssignment/MathOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class MathOperations
+    {
+        public class MathOperations
+        {
+            public void DivideByTwo(int number)
+            {
+                // Divide the number by 2 and display the result
+                int result = number / 2;
+                Console.WriteLine($"Result of division by 2: {result}");
+            }
+        }
+    }
+}
