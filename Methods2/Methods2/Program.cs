fecb5faf4d99e0b3ebf80bdc9c528350b6ad15a0
caduque0906 <caduque0906@gmail.com>
2023-08-22 08:07:58 -0400

    Created instance for MathOperations class

--- Methods2/Methods2/Program.cs
+++ Methods2/Methods2/Program.cs
@@ -10,6 +10,16 @@ namespace Methods2
     {
         static void Main(string[] args)
         {
+            // Create an instance of the MathOperations class
+            MathOperations mathOps = new MathOperations();
+
+            // Call the method and pass in two numbers
+            mathOps.DisplayMathResult(5, 10);
+
+            // Call the method and specify parameters by name
+            mathOps.DisplayMathResult(num1: 8, num2: 15);
+
+            Console.ReadLine();
         }
     }
 }
