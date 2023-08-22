100e2c88d029b205149dbcc4b40c6a959efb750c
caduque0906 <caduque0906@gmail.com>
2023-08-21 21:43:55 -0400

    First Commit

---
+++ CallingMethods/CallingMethods/MathOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class MathOperations
+    {
+        public int Square(int number)
+        {
+            return number * number;
+        }
+
+        public int Double(int number)
+        {
+            return number * 2;
+        }
+
+        public int Increment(int number)
+        {
+            return number + 1;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the MathOperations class
+            MathOperations mathOps = new MathOperations();
+
+            // Ask the user for a number
+            Console.Write("Enter a number for math operations: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // Call each method and display the results
+            int squared = mathOps.Square(userInput);
+            Console.WriteLine($"Square of {userInput}: {squared}");
+
+            int doubled = mathOps.Double(userInput);
+            Console.WriteLine($"Double of {userInput}: {doubled}");
+
+            int incremented = mathOps.Increment(userInput);
+            Console.WriteLine($"Incremented value of {userInput}: {incremented}");
+
+            Console.ReadLine();
+        }
+    }
+}
