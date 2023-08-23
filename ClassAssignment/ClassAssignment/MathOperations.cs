3b0ea9e1de94d0b6dba8589b7d398c50f11e84d9
caduque0906 <caduque0906@gmail.com>
2023-08-23 09:39:08 -0400

    Declard MathOperations as static.

--- ClassAssignment/ClassAssignment/MathOperations.cs
+++ ClassAssignment/ClassAssignment/MathOperations.cs
@@ -6,29 +6,26 @@ using System.Threading.Tasks;
 
 namespace ClassAssignment
 {
-    class MathOperations
+    public static class MathOperations
     {
-        public static class MathOperations
+        public void DivideByTwo(int number)
         {
-            public void DivideByTwo(int number)
-            {
-                // Divide the number by 2 and display the result
-                int result = number / 2;
-                Console.WriteLine($"Result of division by 2: {result}");
-            }
+            // Divide the number by 2 and display the result
+            int result = number / 2;
+            Console.WriteLine($"Result of division by 2: {result}");
+        }
 
-            public void MultiplyByTwo(int number)
-            {
-                // Multiply the number by 2 and display the result
-                int result = number * 2;
-                Console.WriteLine($"Result of multiplication by 2: {result}");
-            }
+        public void MultiplyByTwo(int number)
+        {
+            // Multiply the number by 2 and display the result
+            int result = number * 2;
+            Console.WriteLine($"Result of multiplication by 2: {result}");
+        }
 
-            public void Add(int num1, int num2, out int sum)
-            {
-                // Add two numbers and assign the sum to the output parameter
-                sum = num1 + num2;
-            }
+        public void Add(int num1, int num2, out int sum)
+        {
+            // Add two numbers and assign the sum to the output parameter
+            sum = num1 + num2;
         }
     }
 }
