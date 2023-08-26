339560975d3e615f33df3afa87996e6062dfacb5
caduque0906 <caduque0906@gmail.com>
2023-08-26 16:07:20 -0400

    Created Employee class that inherits from Person class

---
+++ Abstract/Abstract/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            // Display the full name of the employee
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+        }
+    }
+}
