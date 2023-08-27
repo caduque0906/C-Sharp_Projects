6e0dc7e778c3e88b8a6281f18d8c1c8fd62d78c9
caduque0906 <caduque0906@gmail.com>
2023-08-27 14:30:56 -0400

    Employee objects created here

--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -10,6 +10,20 @@ namespace Operators
     {
         static void Main(string[] args)
         {
+            // Create two employee objects
+            Employee employee1 = new Employee { Id = 1, FirstName = "John", LastName = "Doe" };
+            Employee employee2 = new Employee { Id = 2, FirstName = "Jane", LastName = "Smith" };
+
+            // Compare employee objects using overloaded operators
+            bool areEqual = employee1 == employee2;
+            bool areNotEqual = employee1 != employee2;
+
+            // Display comparison results
+            Console.WriteLine($"Employee objects are equal: {areEqual}");
+            Console.WriteLine($"Employee objects are not equal: {areNotEqual}");
+
+            Console.ReadLine();
+
         }
     }
 }
