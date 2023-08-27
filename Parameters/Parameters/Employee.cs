6c322639e172ce32f328e293e4b6f03ec6ea5ef0
caduque0906 <caduque0906@gmail.com>
2023-08-27 14:46:25 -0400

    Employee class with generic type and a "things" property

---
+++ Parameters/Parameters/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+    // Employee class now takes a generic type parameter
+    class Employee<T>
+    {
+        public List<T> Things { get; set; } // Property to hold a generic list of things
+
+        // Constructor to initialize the Things property
+        public Employee()
+        {
+            Things = new List<T>();
+        }
+    }
+}
