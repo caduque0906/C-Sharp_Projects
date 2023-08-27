4d49770c33fa48a17402b09c2687b77002e84264
caduque0906 <caduque0906@gmail.com>
2023-08-27 14:56:07 -0400

    Main entry point of the application

--- ParsingEnums/ParsingEnums/Program.cs
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -17,4 +17,34 @@ namespace ParsingEnums
         Friday,
         Saturday
     }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter the current day of the week: ");
+                string userInput = Console.ReadLine();
+
+                // Convert user input to the DaysOfWeek enum
+                DaysOfWeek currentDay;
+                bool parseSuccess = Enum.TryParse(userInput, out currentDay);
+
+                if (!parseSuccess)
+                {
+                    throw new Exception("Invalid day input");
+                }
+
+                // Print the current day
+                Console.WriteLine($"You entered: {currentDay}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            Console.ReadLine();
+        }
+    }
 }
