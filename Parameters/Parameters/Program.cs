be09fdd9ae515f1e76c548123fdbd92233f61a14
caduque0906 <caduque0906@gmail.com>
2023-08-27 14:47:09 -0400

    Instantiate Employee objects and print their "Things"

--- Parameters/Parameters/Program.cs
+++ Parameters/Parameters/Program.cs
@@ -10,6 +10,31 @@ namespace Parameters
     {
         static void Main(string[] args)
         {
+            // Instantiate an Employee object with type "string"
+            Employee<string> stringEmployee = new Employee<string>();
+            stringEmployee.Things.Add("Apple");
+            stringEmployee.Things.Add("Banana");
+            stringEmployee.Things.Add("Cherry");
+
+            // Instantiate an Employee object with type "int"
+            Employee<int> intEmployee = new Employee<int>();
+            intEmployee.Things.Add(1);
+            intEmployee.Things.Add(2);
+            intEmployee.Things.Add(3);
+
+            // Loop through and print all Things for stringEmployee
+            Console.WriteLine("Things in stringEmployee:");
+            foreach (string thing in stringEmployee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            // Loop through and print all Things for intEmployee
+            Console.WriteLine("\nThings in intEmployee:");
+            foreach (int thing in intEmployee.Things)
+            {
+                Console.WriteLine(thing);
+            }
         }
     }
 }
