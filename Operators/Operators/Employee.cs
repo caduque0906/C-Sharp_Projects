c6cd0290c1138cc2d83da9564b502115a58bd88d
caduque0906 <caduque0906@gmail.com>
2023-08-27 14:29:34 -0400

    Class to represent the employee

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Overloading the equality operator "=="
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            // Check if both objects are null or reference the same object
+            if (ReferenceEquals(employee1, employee2))
+                return true;
+
+            // If either object is null, return false
+            if ((object)employee1 == null || (object)employee2 == null)
+                return false;
+
+            // Compare the Id properties for equality
+            return employee1.Id == employee2.Id;
+        }
+
+        // Overloading the inequality operator "!="
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return !(employee1 == employee2);
+        }
+    }
+}
