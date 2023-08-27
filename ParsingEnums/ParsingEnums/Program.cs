29c19245e1b1978f323b9d507162ec91ec9013b6
caduque0906 <caduque0906@gmail.com>
2023-08-27 14:55:11 -0400

    An enum called DaysOfWeek

---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    // Enum to represent the days of the week
+    enum DaysOfWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
