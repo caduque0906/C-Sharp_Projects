d3632c6f30534ffa95a2b70e38dbb9e62f6a98a9
caduque0906 <caduque0906@gmail.com>
2023-08-29 22:22:42 -0400

    created public static class

---
+++ ClassAssignment/ClassAssignment/Static.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    public static class StaticClass
+    {
+        public static void StaticMethod()
+        {
+            Console.WriteLine("This is a static method in a static class.");
+        }
+    }
+}
