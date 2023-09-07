93badbc5261a0748558c712559195ce0eae7229c
caduque0906 <caduque0906@gmail.com>
2023-09-07 15:11:10 -0400

    First commit. Changed namespace from DateTime to DateTime App.

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step 1: Print the current date and time to the console
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine($"Current date and time: {currentDateTime}");
+
+            // Step 2: Ask the user for a number
+            Console.Write("Enter a number: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            // Step 3: Calculate and print the time in X hours
+            DateTime futureDateTime = currentDateTime.AddHours(userNumber);
+            Console.WriteLine($"In {userNumber} hours, it will be: {futureDateTime}");
+
+            Console.ReadLine();
+        }
+    }
+}
