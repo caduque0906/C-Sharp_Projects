b54e7281f982a41fb948c5fb3bd337f4f9b82a6f
caduque0906 <caduque0906@gmail.com>
2023-09-08 16:42:53 -0400

    Changed comments to be cleaner

--- Constructor/Constructor/Example.cs
+++ Constructor/Constructor/Example.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace Constructor
 {
-    // Step 1: Create a class named "Example"
+    // Create a class named "Example"
     public class Example
     {
-        // Step 2: Create a const variable
+        // Create a const variable
         public const int MaxValue = 100;
 
-        // Step 3: Create a variable using the keyword "var"
+        // Create a variable using the keyword "var"
         public var DynamicVariable = "This is a dynamically typed variable";
 
-        // Step 4: Chain two constructors together
+        // Chain two constructors together
         public Example()
             : this("Default Constructor")
         {
