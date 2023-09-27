68595c638fa55fc373983a8f1ea750e3974747a4
caduque0906 <caduque0906@gmail.com>
2023-09-26 21:33:53 -0400

    N/A

--- Challenge_2/Challenge_2/Program.cs
+++ Challenge_2/Challenge_2/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Reflection.Metadata;
+
+namespace Challenge_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new StudentContext())
+            {
+                Student newStudent = new Student { StudentName = "John Doe" };
+                context.Students.Add(newStudent);
+                context.SaveChanges();
+            }
+
+            Console.WriteLine("Student added successfully.");
+        }
+
+        public class Student
+        {
+            public int StudentID { get; set; }
+            public string StudentName { get; set; }
+        }
+
+        public class StudentContext : DbContext
+        {
+            public StudentContext(DbContextOptions<StudentContext> options) : base(options)
+            {
+            }
+            public DbSet<Student> Students { get; set; }
+        }
+    }
+}
